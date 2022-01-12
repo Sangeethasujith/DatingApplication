65d72741319deac9124558d370cf04a40d28c3bf
Sangeetha <41256616+Sangeethasujith@users.noreply.github.com>
2022-01-12 20:00:52 +0530

    authentication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'The Dating App';
-  users:any;
+  users:any; 
 
   constructor(private http:HttpClient){}
 
